19155f62d2aa8656744cd87b9c4b3b7d7d321378
kylin <whenhan@foxmail.com>
2016-03-10 18:02:12 +0800

    * 原生Ajax

---
+++ js/ajax.js
@@ -0,0 +1,29 @@
+function createXHR() {
+    if (typeof XMLHttpRequest != "undefined") {
+        return new XMLHttpRequest();
+    } else if (typeof ActiveXObject != "undefined") {
+        if (typeof arguments.callee.activeXString != "string") {
+            var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0",
+                    "MSXML2.XMLHttp"],
+                i, len;
+            for (i = 0, len = versions.length; i < len; i++) {
+                try {
+                    new ActiveXObject(versions[i]);
+                    arguments.callee.activeXString = versions[i];
+                    break;
+                } catch (ex) {
+                    //跳过
+                }
+            }
+        }
+        return new ActiveXObject(arguments.callee.activeXString);
+    } else {
+        throw new Error("No XHR object available.");
+    }
+}
+
+// 用法
+
+var xhr = createXHR();
+
+xhr.open("get", "example.php", false);
