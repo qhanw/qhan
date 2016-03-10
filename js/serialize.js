34ae05597b1b80f882f883df6ef20131368dc327
kylin <whenhan@foxmail.com>
2016-03-10 18:04:27 +0800

    * 表单序列化

--- js/serialize.js
+++ js/serialize.js
@@ -1,4 +1,4 @@
-function serialize(form){
+function serialize(form) {
     var parts = [],
         field = null,
         i,
@@ -7,17 +7,17 @@ function serialize(form){
         optLen,
         option,
         optValue;
-    for (i=0, len=form.elements.length; i < len; i++){
+    for (i = 0, len = form.elements.length; i < len; i++) {
         field = form.elements[i];
-        switch(field.type){
+        switch (field.type) {
             case "select-one":
             case "select-multiple":
-                if (field.name.length){
-                    for (j=0, optLen = field.options.length; j < optLen; j++){
+                if (field.name.length) {
+                    for (j = 0, optLen = field.options.length; j < optLen; j++) {
                         option = field.options[j];
-                        if (option.selected){
+                        if (option.selected) {
                             optValue = "";
-                            if (option.hasAttribute){
+                            if (option.hasAttribute) {
                                 optValue = (option.hasAttribute("value") ?
                                     option.value : option.text);
                             } else {
@@ -38,13 +38,13 @@ function serialize(form){
                 break;
             case "radio": //单选按钮
             case "checkbox": //复选框
-                if (!field.checked){
+                if (!field.checked) {
                     break;
                 }
             /* 执行默认操作 */
             default:
-            //不包含没有名字的表单字段
-                if (field.name.length){
+                //不包含没有名字的表单字段
+                if (field.name.length) {
                     parts.push(encodeURIComponent(field.name) + "=" +
                         encodeURIComponent(field.value));
                 }
