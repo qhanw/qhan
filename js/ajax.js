97685ca124a6dd33c14711f515b72f536a62a3a2
kylin <whenhan@foxmail.com>
2016-03-16 09:37:12 +0800

    * AJAX 性能优化，惰情载入

--- js/ajax.js
+++ js/ajax.js
@@ -43,7 +43,7 @@ function createXHR1() {
         }
     } else if (typeof ActiveXObject != 'undefined') {
         createXHR1 = function () {
-            if (typeof arguments.callee.activeXString != 'String') {
+            if (typeof arguments.callee.activeXString != 'string') {
                 var versions = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'], i;
                 for (i = 0; i < versions.length; i++) {
                     try {
@@ -76,7 +76,7 @@ var createXHR2 = (function () {
         }
     } else if (typeof ActiveXObject != 'undefined') {
         return function () {
-            if (typeof arguments.callee.activeXString != 'String') {
+            if (typeof arguments.callee.activeXString != 'string') {
                 var versions = ['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp'], i;
                 for (i = 0; i < versions.length; i++) {
                     try {
@@ -96,38 +96,6 @@ var createXHR2 = (function () {
         }
     }
 })();
-
-
-(function () {
-    var ie = !!(window.attachEvent && !window.opera);
-    var wk = /webkit\/(\d+)/i.test(navigator.userAgent) && (RegExp.$1 < 525);
-    var fn = [];
-    var run = function () {
-        for (var i = 0; i < fn.length; i++) fn[i]();
-    };
-    var d = document;
-    d.ready = function (f) {
-        if (!ie && !wk && d.addEventListener)
-            return d.addEventListener('DOMContentLoaded', f, false);
-        if (fn.push(f) > 1) return;
-        if (ie)
-            (function () {
-                try {
-                    d.documentElement.doScroll('left');
-                    run();
-                }
-                catch (err) {
-                    setTimeout(arguments.callee, 0);
-                }
-            })();
-        else if (wk)
-            var t = setInterval(function () {
-                if (/^(loaded|complete)$/.test(d.readyState))
-                    clearInterval(t), run();
-            }, 0);
-    };
-})();
-
 
 // 用法
 
