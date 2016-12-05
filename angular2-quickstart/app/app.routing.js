41def13068e071006a1e5456c3a7ba8364123c65
kylin <whenhan@foxmail.com>
2016-12-05 09:34:24 +0800

    * Angular2 Globals Update

---
+++ angular2-quickstart/app/app.routing.js
@@ -0,0 +1,13 @@
+"use strict";
+var router_1 = require('@angular/router');
+var heroes_component_1 = require('./components/heroes.component');
+var dashboard_component_1 = require('./components/dashboard.component');
+var hero_detail_component_1 = require('./components/hero-detail.component');
+var appRoutes = [
+    { path: 'heroes', component: heroes_component_1.HeroesComponent },
+    { path: 'dashboard', component: dashboard_component_1.DashboardComponent },
+    { path: 'detail/:id', component: hero_detail_component_1.HeroDetailComponent },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
+exports.routing = router_1.RouterModule.forRoot(appRoutes);
+//# sourceMappingURL=app.routing.js.map
---
+++ angular2-quickstart/app/hero.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by whenh on 2016/9/16.
+ */
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
