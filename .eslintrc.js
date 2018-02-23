38be613adbcfcfd459beaf145d41d4ac64882217
kylin <whenhan@foxmail.com>
2018-02-23 11:06:03 +0800

    * 配置项目Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-const-assign": [2]
-    }
+    "extends": "airbnb"
 };
