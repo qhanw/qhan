b6366044e349c1783cddfd19c1e75903b89de5aa
kylin <whenhan@foxmail.com>
2018-05-31 17:53:39 +0800

    * 侧边栏导航

---
+++ Example/blog/src/components/SideBar/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "gatsby-link";
+import logo from "../../static/logo.svg";
+import IconSakura from "../Icons/sakura";
+
+const SideBar = ({ siteMetadata }) => (
+  <div className="sidebar pure-u-1 pure-u-md-1-4">
+    <div className="header">
+      <h1 className="brand-title">{siteMetadata.title}</h1>
+      <h2 className="brand-tagline">
+        Fear can hold you prisoner. Hope cna set you free.
+      </h2>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link className="pure-button" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item" style={{ marginLeft: 8 }}>
+            <Link className="pure-button" to="/archives">
+              Archives
+            </Link>
+          </li>
+          <li className="nav-item" style={{ marginLeft: 8 }}>
+            <Link className="pure-button" to="/about">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+);
+
+export default SideBar;
