4754c32ca2f7c5a1660237d511e18b7b624e0178
kylin <whenhan@foxmail.com>
2018-05-31 17:18:08 +0800

    * 添加相关依赖

--- Example/blog/src/layouts/index.js
+++ Example/blog/src/layouts/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
-
 import Header from "../components/Header";
+import "purecss/build/pure.css";
 import "./index.scss";
 import "./common.scss";
 
