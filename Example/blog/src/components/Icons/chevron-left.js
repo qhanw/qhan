e5c20a7f5535f148c090583f4b6ae4f0f98b6a8e
kylin <whenhan@foxmail.com>
2018-05-31 17:16:30 +0800

    * Add Direction Icons

---
+++ Example/blog/src/components/Icons/chevron-left.js
@@ -0,0 +1,12 @@
+import React from "react";
+import IconBase from "react-icon-base";
+
+export default class Icon extends React.Component {
+  render() {
+    return (
+      <IconBase viewBox="0 0 512 512" {...this.props}>
+        <path d="M238.475 475.535l7.071-7.07c4.686-4.686 4.686-12.284 0-16.971L50.053 256 245.546 60.506c4.686-4.686 4.686-12.284 0-16.971l-7.071-7.07c-4.686-4.686-12.284-4.686-16.97 0L10.454 247.515c-4.686 4.686-4.686 12.284 0 16.971l211.051 211.05c4.686 4.686 12.284 4.686 16.97-.001z" />
+      </IconBase>
+    );
+  }
+}
---
+++ Example/blog/src/components/Icons/chevron-right.js
@@ -0,0 +1,12 @@
+import React from "react";
+import IconBase from "react-icon-base";
+
+export default class Icon extends React.Component {
+  render() {
+    return (
+      <IconBase viewBox="0 0 512 512" {...this.props}>
+        <path d="M17.525 36.465l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L205.947 256 10.454 451.494c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l211.051-211.05c4.686-4.686 4.686-12.284 0-16.971L34.495 36.465c-4.686-4.687-12.284-4.687-16.97 0z" />
+      </IconBase>
+    );
+  }
+}
