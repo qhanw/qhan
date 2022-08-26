6f8a3529fce589c669b502d1668ecd1469587e4c
qhan <whenhan@foxmail.com>
2022-08-27 00:52:38 +0800

    refactor(site): recover SVG file and delete name

--- site/blog-gatsby/src/components/Header/index.tsx
+++ site/blog-gatsby/src/components/Header/index.tsx
@@ -53,9 +53,6 @@ const Header = ({ siteMetadata }: any) => {
               </Link>
               {/* {siteMetadata.title} */}
             </div>
-            <div className="text-2xl uppercase text-gray-500 md:hidden">
-              Qhan
-            </div>
             <div className="-mr-2 -my-2 md:hidden">
               <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                 <span className="sr-only">Open menu</span>
