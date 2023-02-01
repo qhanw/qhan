a5ffe075f068a4af19a45c697930ccfe5c95a144
qhan <whenhan@foxmail.com>
2023-02-01 18:05:11 +0800

    feat(rust): improve command line tools

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,15 +1,18 @@
-use std::{env, fs};
-
+use minigrep::Config;
+use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    println!("With text: \n{contents}");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
