656494f610f9cb7a6a49090d160dc5e4eaca75e0
qhan <whenhan@foxmail.com>
2023-02-01 18:26:43 +0800

    feat(rust): add test case for command line tools

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -8,8 +8,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
