2f185560f0ec65738898a1f93592d2fa4fe915f2
qhan <whenhan@foxmail.com>
2023-02-02 14:01:12 +0800

    feat(rust): control error mesaage output

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -12,7 +12,7 @@ fn main() {
     // println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
