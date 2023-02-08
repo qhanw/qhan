cba63e2e8a49909d596532e76f4ba15b62beea2c
qhan <whenhan@foxmail.com>
2023-02-08 18:29:22 +0800

    feat(rust): learn the thread of rust

---
+++ rust/rs-16/src/main-base.rs
@@ -0,0 +1,26 @@
+use std::{thread, time::Duration};
+// fn main() {
+//     let handle = thread::spawn(|| {
+//         for i in 1..10 {
+//             println!("hi number {} from the spawned thread!", i);
+//             thread::sleep(Duration::from_millis(1));
+//         }
+//     });
+
+//     for i in 1..5 {
+//         println!("hi number {} from the main thread!", i);
+//         thread::sleep(Duration::from_millis(1));
+//     }
+
+//     handle.join().unwrap();
+// }
+
+fn main() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+}
