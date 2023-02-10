7e9a30e03847d9a3c5cad9b0f58e11dd2680ff89
qhan <whenhan@foxmail.com>
2023-02-10 13:59:37 +0800

    feat(rust): learn the oop of rust

--- rust/blog/src/lib.rs
+++ rust/blog/src/lib.rs
@@ -1,72 +1,41 @@
 pub struct Post {
-    state: Option<Box<dyn State>>,
     content: String,
 }
+
+pub struct DraftPost {
+    content: String,
+}
+
 impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
+    pub fn new() -> DraftPost {
+        DraftPost {
             content: String::new(),
         }
     }
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPost {
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text);
     }
-
-    pub fn content(&self) -> &str {
-        self.state.as_ref().unwrap().content(self)
-    }
-
-    pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.request_review())
-        }
-    }
-
-    pub fn approve(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
         }
     }
 }
 
-trait State {
-    fn request_review(self: Box<Self>) -> Box<dyn State>;
-    fn approve(self: Box<Self>) -> Box<dyn State>;
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        ""
-    }
+pub struct PendingReviewPost {
+    content: String,
 }
 
-struct Draft {}
-impl State for Draft {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        Box::new(PendingReview {})
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-}
-
-struct PendingReview {}
-impl State for PendingReview {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        Box::new(Published {})
-    }
-}
-
-struct Published {}
-impl State for Published {
-    fn request_review(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn approve(self: Box<Self>) -> Box<dyn State> {
-        self
-    }
-    fn content<'a>(&self, post: &'a Post) -> &'a str {
-        &post.content
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
     }
 }
--- rust/blog/src/main.rs
+++ rust/blog/src/main.rs
@@ -1,14 +1,27 @@
 use blog::Post;
 
+// lib state
+// fn main() {
+//     let mut post = Post::new();
+
+//     post.add_text("I ate a salad for lunch today");
+//     assert_eq!("", post.content());
+
+//     post.request_review();
+//     assert_eq!("", post.content());
+
+//     post.approve();
+//     assert_eq!("I ate a salad for lunch today", post.content());
+// }
+
 fn main() {
     let mut post = Post::new();
 
     post.add_text("I ate a salad for lunch today");
-    assert_eq!("", post.content());
 
-    post.request_review();
-    assert_eq!("", post.content());
+    let post = post.request_review();
 
-    post.approve();
+    let post = post.approve();
+
     assert_eq!("I ate a salad for lunch today", post.content());
 }
