2b8385898f55ec92fb7226450d2fc95eaf52968b
qhan <whenhan@foxmail.com>
2023-05-29 13:00:04 +0800

    chore(site): remove emotion from gatsby

--- site/blog-gatsby/gatsby-config.ts
+++ site/blog-gatsby/gatsby-config.ts
@@ -11,9 +11,9 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  jsxRuntime: "automatic",
   plugins: [
     "gatsby-plugin-pnpm",
-    "gatsby-plugin-emotion",
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
