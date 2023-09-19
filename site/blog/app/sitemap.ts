9ed35b4e202bc760b62dd33787aedfbef99fae5c
qhan <whenhan@foxmail.com>
2023-09-19 17:03:06 +0800

    feat(site/blog): change sitemap

--- site/blog/app/sitemap.ts
+++ site/blog/app/sitemap.ts
@@ -1,6 +1,25 @@
 import { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+import { getAllPosts } from "@/lib/posts";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  // 远程获取博客列表
+  const posts = await getAllPosts();
+
+  // 转换为博客的 sitemap
+  const maps: MetadataRoute.Sitemap = posts.map((post) => ({
+    url: `https://qhan.wang/posts/${post.slug}`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 0.6,
+  }));
+
+  // // 加入本地的其他路由页面
+  // const routes = ["", "/about", "/blog"].map((route) => ({
+  //   url: `https://xxx.com${route}`,
+  //   lastModified: new Date().toISOString(),
+  // }));
+
   return [
     {
       url: "https://qhan.wang",
@@ -9,16 +28,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://qhan.wang/stories",
+      url: "https://qhan.wang/posts",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: "https://qhan.wang.com/blog",
+      url: "https://qhan.wang.com/projects",
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.5,
+      priority: 0.8,
     },
+    ...maps,
   ];
 }
