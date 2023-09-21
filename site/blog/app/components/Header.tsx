e3fce26064b4272000664b9a9132cf91c960b488
qhan <whenhan@foxmail.com>
2023-09-21 17:08:31 +0800

    feat(site/blog): adjust header style

--- site/blog/app/components/Header.tsx
+++ site/blog/app/components/Header.tsx
@@ -1,8 +1,6 @@
-"use client";
 import { Fragment } from "react";
 import Link from "next/link";
 import clsx from "clsx";
-import { Popover, Transition } from "@headlessui/react";
 
 const nav = [
   {
@@ -31,7 +29,10 @@ const nav = [
     name: "Bizk",
     href: "http://bizk.qhan.wang/",
     icon: (props: any) => (
-      <span {...props} className={clsx("i-heroicons:wrench-screwdriver", props.className)} />
+      <span
+        {...props}
+        className={clsx("i-heroicons:wrench-screwdriver", props.className)}
+      />
     ),
   },
   // { name: "Archives", href: "/archives", icon: ArchiveBoxIcon },
@@ -40,103 +41,38 @@ const nav = [
 
 export default () => (
   <Fragment>
-    <Popover className="sticky top-0 z-40 w-full backdrop-blur transition-colors duration-500 bg-white/95 supports-backdrop-blur:bg-white/60 ">
-      <div className="px-4 sm:px-6">
-        <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
-          <div className="flex justify-start md:w-0 md:flex-1">
-            <Link href="/" className="inline-flex items-center">
-              <span className="sr-only">Workflow</span>
-              <span className="i-logos-sakura w-8 h-8 logo-rotate text-brand" />
-              <span className="text-xl ml-2 font-medium">Qhan W</span>
-            </Link>
-          </div>
-          <div className="-mr-2 -my-2 md:hidden">
-            <Popover.Button className="rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100">
-              <span className="sr-only">Open menu</span>
-              <span className="i-heroicons:bars-3 h-6 w-6" aria-hidden="true" />
-            </Popover.Button>
-          </div>
-          <Popover.Group as="nav" className="hidden md:flex space-x-10">
-            {nav.map((item) => (
-              <Link
-                key={item.name}
-                href={item.href}
-                className="text-base font-medium inline-flex items-center text-gray-500 hover:text-gray-900"
-                {...(/^http(s?):\/\//.test(item.href)
-                  ? { target: "_blank" }
-                  : {})}
-              >
-                <item.icon
-                  className="flex-shrink-0 h-4 w-4 mr-3 text-brand"
-                  aria-hidden="true"
-                />
-                {item.name}
-              </Link>
-            ))}
-          </Popover.Group>
-          {/* <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              <a
-                href="#"
-                className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-              >
-                Sign in
-              </a>
-              <a
-                href="#"
-                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Sign up
-              </a>
-            </div> */}
+    <div className="px-4 sm:px-6 sticky top-0 z-40 w-full backdrop-blur transition-colors duration-500 bg-white/95 supports-backdrop-blur:bg-white/60 ">
+      <div className="flex justify-between items-center py-4 md:justify-start md:space-x-10">
+        <div className="flex justify-start md:w-0 md:flex-1">
+          <Link href="/" className="inline-flex items-center">
+            <span className="sr-only">Workflow</span>
+            <span className="i-logos-sakura w-6 h-6 logo-rotate text-brand" />
+            <span className="text-lg ml-2 font-medium text-gray-900">
+              Qhan W
+            </span>
+          </Link>
         </div>
-      </div>
 
-      <Popover.Panel
-        focus
-        className="absolute top-0 z-10 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-      >
-        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-          <div className="pt-5 pb-6 px-5">
-            <div className="flex items-center justify-between">
-              <Link href="/" className="outline-none">
-                <span className="i-logos-sakura w-8 h-8 logo-rotate text-brand" />
-              </Link>
-              <div className="-mr-2">
-                <Popover.Button className="rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100">
-                  <span className="sr-only">Close menu</span>
-                  <span
-                    className="i-heroicons:x-mark h-6 w-6"
-                    aria-hidden="true"
-                  />
-                </Popover.Button>
-              </div>
-            </div>
-            <div className="mt-6">
-              <nav className="grid gap-y-8">
-                {nav.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                    {...(/^http(s?):\/\//.test(item.href)
-                      ? { target: "_blank" }
-                      : {})}
-                  >
-                    <item.icon
-                      className="flex-shrink-0 h-6 w-6 text-brand"
-                      aria-hidden="true"
-                    />
-                    <span className="ml-3 text-base font-medium text-gray-900">
-                      {item.name}
-                    </span>
-                  </Link>
-                ))}
-              </nav>
-            </div>
-          </div>
-        </div>
-      </Popover.Panel>
-    </Popover>
+        <nav className="flex space-x-8 md:space-x-10">
+          {nav.map((item) => (
+            <Link
+              key={item.name}
+              href={item.href}
+              className="text-base font-medium inline-flex items-center space-x-2 text-gray-500 hover:text-gray-900"
+              {...(/^http(s?):\/\//.test(item.href)
+                ? { target: "_blank" }
+                : {})}
+            >
+              <item.icon
+                className="flex-shrink-0 h-4 w-4 text-brand"
+                aria-hidden="true"
+              />
+              <span className="hidden md:flex">{item.name}</span>
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </div>
     <div className="header-line-shadow" />
   </Fragment>
 );
