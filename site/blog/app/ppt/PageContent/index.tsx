9235cd995f7d8eeabdebb0d9c1e7bca4356a876d
qhan <whenhan@foxmail.com>
2023-09-25 18:01:09 +0800

    feat(site/blog): add content of ppt

--- site/blog/app/ppt/PageContent/index.tsx
+++ site/blog/app/ppt/PageContent/index.tsx
@@ -1,43 +1,17 @@
 "use client";
 import Script from "next/script";
+import Image from "next/image";
 
 import "./common.scss";
 import "./impress.scss";
 
+import IMG_1 from "./images/IMG_1.jpg";
+import IMG_2 from "./images/IMG_2.jpg";
+import IMG_3 from "./images/IMG_3.jpg";
+
 export default function PPT() {
   return (
     <>
-      {/*
-    
-    Now that's the core element used by impress.js.
-    
-    That's the wrapper for your presentation steps. In this element all the impress.js magic happens.
-    It doesn't have to be a `<div>`. Only `id` is important here as that's how the script find it.
-    
-    Often you don't need to use any attributes here, but for educational purposes I have used all of them below.
-
-    To change the duration of the transition between slides use `data-transition-duration="2000"` giving it
-    a number of ms. It defaults to 1000 (1s).
-    
-    When authoring impress.js presentations, you should target some screen size, which you can define here.
-    The default is 1024 x 768. You should write your CSS as if this is the screen size used for the
-    presentation. When you present your presentation on a screen (or browser window) of different size,
-    impress.js will automatically scale the presentation to fit the screen. The minimum and maximum limits
-    to this scaling can also be defined here.
-    
-    NOTE: I intend to change the defaults to target HD screens in 2021. So you may want to make a habit
-    of explicitly defining these attributes for now, to avoid any disruption when the defaults change.
-    
-    You can also control the perspective with `data-perspective="500"` giving it a number of pixels.
-    It defaults to 1000. You can set it to 0 if you don't want any 3D effects.
-    If you are willing to change this value make sure you understand how CSS perspective works:
-    https://developer.mozilla.org/en/CSS/perspective
-    
-    Plugins:
-    
-    We set the default time for autoplay plugin to 7 seconds. Autoplay will automatically advance
-    to next slide after a timeout expires.
-*/}
       <div
         id="impress"
         data-transition-duration="1000"
@@ -48,26 +22,6 @@ export default function PPT() {
         data-perspective="1000"
         data-autoplay="7"
       >
-        {/*
-        
-        Here is where interesting thing start to happen.
-        
-        Each step of the presentation should be an element inside the `#impress` with a className name
-        of `step`. These step elements are positioned, rotated and scaled by impress.js, and
-        the 'camera' shows them on each step of the presentation.
-        
-        Positioning information is passed through data attributes.
-        
-        In the example below we only specify x and y position of the step element with `data-x="-1000"`
-        and `data-y="-1500"` attributes. This means that **the center** of the element (yes, the center)
-        will be positioned in point x = -1000px and y = -1500px of the presentation 'canvas'.
-        
-        It will not be rotated or scaled.
-        
-        --------
-        Plugins: For first slide, set the autoplay time to a custom 10 seconds.
-        
-    */}
         <div
           id="bored"
           className="step slide"
@@ -75,40 +29,18 @@ export default function PPT() {
           data-y="-1500"
           data-autoplay="10"
         >
-          <q>
-            Aren’t you just <b>bored</b> with all those slides-based
-            presentations?
-          </q>
+          <Image src={IMG_1} alt="" />
+          <div>Are You Ready?</div>
         </div>
 
-        {/*
-        
-        The `id` attribute of the step element is used to identify it in the URL, but it's optional.
-        If it is not defined, it will get a default value of `step-N` where N is a number of slide.
-        
-        So in the example below it'll be `step-2`.
-        
-        The hash part of the url when this step is active will be `#/step-2`.
-        
-        You can also use `#step-2` in a link, to point directly to this particular step.
-        
-        Please note, that while `#/step-2` (with slash) would also work in a link it's not recommended.
-        Using classic `id`-based links like `#step-2` makes these links usable also in fallback mode.
-        
-    */}
         <div className="step slide" data-x="0" data-y="-1500">
-          <q>
-            Don’t you think that presentations given{" "}
-            <strong>in modern browsers</strong> shouldn’t{" "}
-            <strong>copy the limits</strong> of ‘classic’ slide decks?
-          </q>
+          <Image src={IMG_2} alt="" />
+          <div>Are You Ready?</div>
         </div>
 
         <div className="step slide" data-x="1000" data-y="-1500">
-          <q>
-            Would you like to <strong>impress your audience</strong> with{" "}
-            <strong>stunning visualization</strong> of your talk?
-          </q>
+          <Image src={IMG_3} alt="" />
+          <div>Are You Ready?</div>
         </div>
 
         {/*
