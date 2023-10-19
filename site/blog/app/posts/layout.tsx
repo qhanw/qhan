55e0f047cbc72aad0971f95ea5b7dbf204526be3
qhan <whenhan@foxmail.com>
2023-10-19 12:05:28 +0800

    feat(site/blog): add nextjs-toploader

--- site/blog/app/posts/layout.tsx
+++ site/blog/app/posts/layout.tsx
@@ -1,3 +1,4 @@
+import NextTopLoader from "nextjs-toploader";
 import Footer from "@/app/components/Footer";
 export default function PostsLayout({
   children,
@@ -6,6 +7,12 @@ export default function PostsLayout({
 }) {
   return (
     <>
+      <NextTopLoader
+        color="rgba(156,163,175,0.45)"
+        height={2}
+        showSpinner={false}
+        shadow={false}
+      />
       {children}
       <Footer className="prose mx-auto" />
     </>
