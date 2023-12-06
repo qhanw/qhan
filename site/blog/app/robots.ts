976095e717bffa3994cbd61b7783db4c458a83da
qhan <whenhan@foxmail.com>
2023-12-06 14:51:43 +0800

    feat(site/blog): update sitemap.xml

--- site/blog/app/robots.ts
+++ site/blog/app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      // allow: "/",
       //  disallow: "/private/",
     },
     sitemap: "https://qhan.wang/sitemap.xml",
