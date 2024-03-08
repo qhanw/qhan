565db59fabfb70c82cc99aa805c123abc5c2330c
qhan <whenhan@foxmail.com>
2024-03-08 16:51:15 +0800

    feat(site/blog): update dependencies and replace github alerts

--- site/blog/app/(web)/posts/[slug]/MDXContent.tsx
+++ site/blog/app/(web)/posts/[slug]/MDXContent.tsx
@@ -2,9 +2,7 @@ import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 
 import remarkToc from "remark-toc";
 import remarkGfm from "remark-gfm";
-import remarkGithubAlerts from "remark-github-alerts";
-
-import rehypeRaw from "rehype-raw";
+import remarkGithubAlerts from "remark-gh-alerts";
 
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
@@ -14,10 +12,8 @@ import rehypeShiki from "@shikijs/rehype";
 import Button from "@/posts-mdx/next-mdx/button";
 import CssGradientBtn from "@/posts-mdx/css-gradient/GradientBtn";
 
-import "remark-github-alerts/styles/github-base.css";
-import "remark-github-alerts/styles/github-colors-light.css"
-
-
+import "remark-gh-alerts/styles/github-base.css";
+import "remark-gh-alerts/styles/github-colors-light.css";
 
 const options: MDXRemoteProps["options"] = {
   mdxOptions: {
@@ -30,11 +26,8 @@ const options: MDXRemoteProps["options"] = {
     rehypePlugins: [
       rehypeSlug,
       rehypeAutolinkHeadings,
-
       // @ts-ignore
       [rehypeShiki, { theme: "nord" }],
-      // @ts-ignore
-      [rehypeRaw, { passThrough: ["mdxjsEsm", "mdxJsxFlowElement"] }],
     ],
   },
 };
