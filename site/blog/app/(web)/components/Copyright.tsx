f140bce8e20d2c33b2c87f88dbd17d1edfb3275c
qhan <whenhan@foxmail.com>
2024-05-10 16:34:17 +0800

    feat(site/blog): add CC BY-NC-SA 4.0

--- site/blog/app/(web)/components/Copyright.tsx
+++ site/blog/app/(web)/components/Copyright.tsx
@@ -1,7 +1,16 @@
 "use client";
 
+import Link from "next/link";
+
 export default () => (
   <div className="text-sm opacity-50 ml-1">
     ©2022-{new Date().getFullYear()} Qhan W. All Rights Reserved.
+    <Link
+      className="text-brand ml-2"
+      target="_blank"
+      href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+    >
+      CC BY-NC-SA 4.0
+    </Link>
   </div>
 );
