b41318f767d88071f97feaaba6aa31cccbf13371
qhan <whenhan@foxmail.com>
2024-10-12 12:18:15 +0800

    feat(site/blog): add middleware

--- site/blog/app/(web)/components/Theme.tsx
+++ site/blog/app/(web)/components/Theme.tsx
@@ -7,11 +7,11 @@ export default function Theme() {
   const isDark = theme === "dark";
 
   return (
-    <a
+    <span
       onClick={() => setTheme(isDark ? "light" : "dark")}
-      className="text-slate-600 dark:text-slate-200 cursor-pointer"
+      className="text-slate-600 dark:text-slate-300 hover:text-slate-800 dark:hover:text-slate-100 cursor-pointer"
     >
       <i className={isDark ? "i-ri:sun-fill" : "i-ri:moon-fill"} />
-    </a>
+    </span>
   );
 }
