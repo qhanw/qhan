e312493e9ef93ac5f4000ec5c8160a48cb950ccc
qhan <whenhan@foxmail.com>
2025-07-29 14:11:13 +0800

    feat(site/blog): adjust sitemap.xml cache stragegy

--- site/blog/app/(web)/lib/utils.ts
+++ site/blog/app/(web)/lib/utils.ts
@@ -4,14 +4,17 @@ import path from "node:path";
 export function getGitLastUpdatedTimeStamp(filePath: string) {
   let lastUpdated;
   try {
-    const timestamp = spawnSync(
+    const buffer = spawnSync(
       "git",
       ["log", "-1", "--format=%at", path.basename(filePath)],
       { cwd: path.dirname(filePath) }
-    ).stdout.toString("utf-8");
+    );
+
+    const timestamp = buffer.stdout?.toString("utf-8");
 
     lastUpdated = timestamp ? new Date(parseInt(timestamp) * 1000) : new Date();
-  } catch {
+  } catch (e) {
+    console.error("Fail to lastUpdated", e);
     /* do not handle for now */
   }
   return lastUpdated;
--- site/blog/app/sitemap.ts
+++ site/blog/app/sitemap.ts
@@ -1,10 +1,8 @@
 import { MetadataRoute } from "next";
-import { connection } from "next/server";
 
 import { getAllPosts } from "@/app/(web)/lib/service";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  await connection();
   // 远程获取博客列表
   const posts = await getAllPosts();
 
@@ -12,7 +10,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   // 转换为博客的 sitemap
   const posts_maps: MetadataRoute.Sitemap = posts.map((post) => ({
-    url: `${domain}/posts/${post.slug}/`,
+    url: `${domain}/posts/${post.slug}`,
     lastModified: post.meta.lastModified?.toISOString(),
     // changeFrequency: "daily",
     // priority: +(0.8 * 0.8).toFixed(2),
@@ -20,7 +18,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   // 加入本地的其他路由页面
   const route_maps: MetadataRoute.Sitemap = ["", "/posts"].map((route) => ({
-    url: domain + route + "/",
+    url: domain + route,
     lastModified: new Date().toISOString(),
     // changeFrequency: "weekly",
     // priority: !route || route === "/" ? 1 : 0.8,
